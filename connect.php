f0e1c1ea12f5c1dbe346fd9775b5c3d296d69a5b
Viktor Ladik <victorladik742@gmail.com>
2020-04-06 22:56:09 +0300

    апа

--- connect.php
+++ connect.php
@@ -5,10 +5,3 @@ $mysqli = new mysqli(
     'oyJJjg54j1',
     'mPjtK2pney'
 );
- // https://gbladik.herokuapp.com/
-    
-    // Username: mPjtK2pney
-    // Database name: mPjtK2pney
-    // Password: oyJJjg54j1
-    // Server: remotemysql.com
-    // Port: 3306
--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 </head>
 
 <body>
+
     <?php
+
 
     // https://gbladik.herokuapp.com/
 
@@ -17,24 +19,20 @@
     // Password: oyJJjg54j1
     // Server: remotemysql.com
     // Port: 3306
+    //SELECT COUNT(*) as count FROM www
 
     include('connect.php');
     include('fun.php');
-
-
-
 
     $result = $mysqli->query('SELECT * FROM `www`');
     while ($row = $result->fetch_object()) {
         echo "<b>" . smile($row->text) . "</b> <i>$row->name</i><br>\n";
     }
 
-
-
-
     $result->free();
     $mysqli->close();
     ?>
+
     <form action="add.php" method="POST">
         <textarea name="text" cols="30" rows="10"></textarea><br>
         <input type="text" name="name"><br>
