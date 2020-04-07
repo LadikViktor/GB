376d6bf073ab8a5aaf2d95ea51ae4f9713b6f046
Viktor Ladik <victorladik742@gmail.com>
2020-04-07 20:23:07 +0300

    bold

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .pageination {
+        .pageination a {
             color: red;
         }
 
         .selectedpage {
-            color: blue;
+            color: blue !important;
             font-weight: bold;
         }
     </style>
